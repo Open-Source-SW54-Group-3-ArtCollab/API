56972ab39ff47512701f4555bef4d45de9fadf05
Gustavo Huilca <u202213983@upc.edu.pe>
2024-05-26 18:08:26 -0500

    feat: inicial commit

---
+++ src/test/java/upc/edu/artcollab/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package upc.edu.artcollab.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
