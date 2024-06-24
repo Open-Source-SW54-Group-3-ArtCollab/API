c54fc658388ebbb18d1d93de9bd35f643a8f9213
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-06-24 01:11:28 -0500

    fix: fixed cors configured

--- src/main/java/upc/edu/artcollab/api/ApiApplication.java
+++ src/main/java/upc/edu/artcollab/api/ApiApplication.java
@@ -3,6 +3,7 @@ package upc.edu.artcollab.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,15 +15,13 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-					.allowedMethods("GET", "POST", "PUT", "DELETE");
-			}
-		};
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer {
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+		}
 	}
+
 
 }
